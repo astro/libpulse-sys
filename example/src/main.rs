18fa776e5d2f30e7daabd78bb8b2490c8433208b
Astro <astro@spaceboyz.net>
2016-01-10 01:13:49 +0100

    add tone-generator example

---
+++ example/src/main.rs
@@ -0,0 +1,40 @@
+extern crate libpulse_sys;
+
+use libpulse_sys::*;
+use std::ptr::{null, null_mut};
+use std::{mem, slice};
+
+const SAMPLES: usize = 240;
+
+fn main() {
+    let ss = pa_sample_spec {
+        format: PA_SAMPLE_S16LE,
+        channels: 1,
+        rate: 48000
+    };
+    let s = unsafe {
+        pa_simple_new(null(),             // Use the default server.
+                      "Rust!".as_ptr() as *const i8,  // Our application's name.
+                      PA_STREAM_PLAYBACK,
+                      null(),             // Use the default device.
+                      "Example".as_ptr() as *const i8,  // Description of our stream.
+                      &ss,                // Our sample format.
+                      null(),             // Use default channel map
+                      null(),             // Use default buffering attributes.
+                      null_mut(),         // Ignore error code.
+                     )
+    };
+    assert!(s != null_mut());
+
+    let buf = unsafe { mem::transmute(pa_xmalloc(2 * SAMPLES)) };
+    loop {
+        let samples: &mut [i16] = unsafe { slice::from_raw_parts_mut(buf, SAMPLES) };
+        for (i, sample) in samples.iter_mut().enumerate() {
+            *sample = i as i16 * 100;
+        }
+        let res = unsafe {
+            pa_simple_write(s, mem::transmute(buf), 2 * SAMPLES, null_mut())
+        };
+        assert!(res == 0);
+    }
+}
